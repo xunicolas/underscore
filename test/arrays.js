082ed8347223e569a82b0fa2542399ede7167db8
Nicolas Xu <xulixiong@hotmail.com>
2017-12-26 14:21:58 +0800

    Add Comment Line for Testing

--- test/arrays.js
+++ test/arrays.js
@@ -221,4 +221,5 @@ $(document).ready(function() {
     equal(_.range(0, -10, -1).join(' '), '0 -1 -2 -3 -4 -5 -6 -7 -8 -9', 'final example in the Python docs');
   });
 //Please ignore my testing changes
+//The second commit
 });
